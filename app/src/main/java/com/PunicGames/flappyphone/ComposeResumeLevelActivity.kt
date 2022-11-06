ec5e5a94626a460112b9bb57ddb3b661a6653340
Miguel Rodriguez <maiguelonflis456@gmail.com>
2022-11-06 19:44:22 +0100

    New buttons
    Nuevos botones y cambiado el sprite de la estrella

--- app/src/main/java/com/PunicGames/flappyphone/ComposeResumeLevelActivity.kt
+++ app/src/main/java/com/PunicGames/flappyphone/ComposeResumeLevelActivity.kt
@@ -1,14 +1,15 @@
 package com.PunicGames.flappyphone
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,7 +17,11 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -25,6 +30,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.PunicGames.flappyphone.ui.theme.FlappyPhoneTheme
+import com.PunicGames.flappyphone.ui.theme.marble
 import java.time.format.TextStyle
 
 class ComposeResumeLevelActivity : ComponentActivity() {
@@ -38,6 +44,7 @@ class ComposeResumeLevelActivity : ComponentActivity() {
     var star3Alpha = 0.2f
     var star4Alpha = 0.2f
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -88,6 +95,7 @@ class ComposeResumeLevelActivity : ComponentActivity() {
         star4Alpha = 1f
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @Preview(showBackground = true, widthDp = 400, heightDp = 800)
     @Composable
     fun ResumeLevelViewContainer(){
@@ -108,33 +116,89 @@ class ComposeResumeLevelActivity : ComponentActivity() {
         )
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @Composable
     fun ExitButton(){
 
 
         val mContext = LocalContext.current
 
-        Button(colors = ButtonDefaults.buttonColors(
-            backgroundColor = Color(red = 0, green = 102, blue =255),contentColor = Color.White),
-            modifier = Modifier
-                .fillMaxSize(),
-            shape = RoundedCornerShape(30.dp),
-            elevation = ButtonDefaults.elevation(
-                defaultElevation = 10.dp,
-                pressedElevation = 15.dp,
-                disabledElevation = 0.dp
-            ),
-            onClick = { mContext.startActivity(Intent(mContext, ComposeMainMenu::class.java))
-            finish()}
-        ) {
-            Text(
-                text = "Exit",
-                fontSize = 40.sp,
-                textAlign = TextAlign.Center
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.btn),
+                contentDescription = "Boton",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(40))
+                    .border(2.dp, Color.Black, RoundedCornerShape(40))
+                    .clickable(
+                        onClick = {
+                            mContext.startActivity(
+                                Intent(
+                                    mContext,
+                                    ComposeLevelSelectionActivity::class.java
+                                )
+                            )
+                        }
+                    )
             )
+
+            val aux = 150f
+
+            val customTypeface = LocalContext.current.resources.getFont(R.font.marblefont)
+
+            val textPaintStroke = Paint().asFrameworkPaint().apply {
+                isAntiAlias = true
+                style = android.graphics.Paint.Style.STROKE
+                textSize = aux
+                color = android.graphics.Color.BLACK
+                strokeWidth = 13f
+                strokeMiter= 10f
+                strokeJoin = android.graphics.Paint.Join.ROUND
+                textAlign = android.graphics.Paint.Align.CENTER
+                typeface = customTypeface
+            }
+
+            val textPaint = Paint().asFrameworkPaint().apply {
+                isAntiAlias = true
+                style = android.graphics.Paint.Style.FILL
+                textSize = aux
+                color = android.graphics.Color.WHITE
+                textAlign = android.graphics.Paint.Align.CENTER
+                typeface = customTypeface
+            }
+
+            Canvas(
+                modifier = Modifier
+                    .fillMaxSize(),
+                onDraw = {
+                    drawIntoCanvas {
+                        it.nativeCanvas.drawText(
+                            "Exit",
+                            size.width / 2,
+                            size.height / 2 + aux / 3,
+                            textPaintStroke
+                        )
+                        it.nativeCanvas.drawText(
+                            "Exit",
+                            size.width / 2,
+                            size.height / 2 + aux / 3,
+                            textPaint
+                        )
+                    }
+                }
+            )
+
         }
+
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @Composable
     fun ResumeLevelContent(){
 
@@ -209,7 +273,7 @@ class ComposeResumeLevelActivity : ComponentActivity() {
                             //.background(Color.Yellow)
                     ) {
                         Image(
-                            painter = painterResource(id = R.drawable.star),
+                            painter = painterResource(id = R.drawable.estrella),
                             contentDescription = "Star",
                             contentScale = ContentScale.FillWidth,
                             modifier = Modifier.fillMaxSize(),
@@ -226,7 +290,7 @@ class ComposeResumeLevelActivity : ComponentActivity() {
                             //.background(Color.Red)
                     ) {
                         Image(
-                            painter = painterResource(id = R.drawable.star),
+                            painter = painterResource(id = R.drawable.estrella),
                             contentDescription = "Star",
                             contentScale = ContentScale.FillWidth,
                             modifier = Modifier.fillMaxSize(),
@@ -243,7 +307,7 @@ class ComposeResumeLevelActivity : ComponentActivity() {
                             //.background(Color.Yellow)
                     ) {
                         Image(
-                            painter = painterResource(id = R.drawable.star),
+                            painter = painterResource(id = R.drawable.estrella),
                             contentDescription = "Star",
                             contentScale = ContentScale.FillWidth,
                             modifier = Modifier.fillMaxSize(),
@@ -260,7 +324,7 @@ class ComposeResumeLevelActivity : ComponentActivity() {
                             //.background(Color.Red)
                     ) {
                         Image(
-                            painter = painterResource(id = R.drawable.star),
+                            painter = painterResource(id = R.drawable.estrella),
                             contentDescription = "Star",
                             contentScale = ContentScale.FillWidth,
                             modifier = Modifier.fillMaxSize(),
--- app/src/main/java/com/PunicGames/flappyphone/ui/theme/Type.kt
+++ app/src/main/java/com/PunicGames/flappyphone/ui/theme/Type.kt
@@ -2,9 +2,15 @@ package com.PunicGames.flappyphone.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.Typeface
 import androidx.compose.ui.unit.sp
+import com.PunicGames.flappyphone.R
+import java.nio.file.attribute.AclEntry.Builder
+
+val marble = FontFamily(Font(R.font.marblefont))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -26,3 +32,4 @@ val Typography = Typography(
     )
     */
 )
+
